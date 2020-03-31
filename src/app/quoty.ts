2f0db765843a499c712d18d114aa793c848a7275
Michelle- <michukimichelle@gmail.com>
2020-03-31 10:52:13 +0300

    changes made

--- src/app/quoty.ts
+++ src/app/quoty.ts
@@ -1,2 +1,4 @@
-export class Quoty {
-}
+export class Quote{
+    constructor(public id:number, public publisher:string, public author:string, public quote: string, public completeDate:Date){
+    }
+  }
